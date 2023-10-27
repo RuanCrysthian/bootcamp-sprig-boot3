41d4de4069f266f6afbf1fcef9e1a30529d22f60
Ruan Ferraz <ruan.ferraz@bethebeth.com.br>
2023-10-27 15:08:52 -0300

    Criacao da Category

---
+++ dscatalog/back-end/dscatalog/src/main/java/com/bootcamp/dscatalog/entities/Category.java
@@ -0,0 +1,34 @@
+package com.bootcamp.dscatalog.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+public class Category implements Serializable {
+  private Long id;
+  private  String name;
+
+  public Category() {}
+
+  public Category(Long id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Category category = (Category) o;
+    return Objects.equals(id, category.id) && Objects.equals(name, category.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name);
+  }
+}
