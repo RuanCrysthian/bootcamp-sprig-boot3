51d0329cbbf7ef7616add423e629b22dfba32d84
Ruan Ferraz <ruan.ferraz@bethebeth.com.br>
2023-10-27 16:01:48 -0300

    h2 Database

--- dscatalog/back-end/dscatalog/src/main/java/com/bootcamp/dscatalog/entities/Category.java
+++ dscatalog/back-end/dscatalog/src/main/java/com/bootcamp/dscatalog/entities/Category.java
@@ -1,5 +1,6 @@
 package com.bootcamp.dscatalog.entities;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,8 +9,14 @@ import java.util.Objects;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+
   private  String name;
 
   public Category() {}
---
+++ dscatalog/back-end/dscatalog/src/main/java/com/bootcamp/dscatalog/services/CategoryService.java
@@ -0,0 +1,19 @@
+package com.bootcamp.dscatalog.services;
+
+import com.bootcamp.dscatalog.entities.Category;
+import com.bootcamp.dscatalog.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+  @Autowired
+  private CategoryRepository repository;
+
+  public List<Category> findAll() {
+    return repository.findAll();
+  }
+}
