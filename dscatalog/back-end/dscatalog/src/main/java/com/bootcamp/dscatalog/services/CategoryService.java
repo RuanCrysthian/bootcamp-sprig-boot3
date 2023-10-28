b5fc30064501f68f86bfbfbb4f82955dd2e28047
Ruan Ferraz <ruan.ferraz@bethebeth.com.br>
2023-10-28 17:27:25 -0300

    PUT Update

--- dscatalog/back-end/dscatalog/src/main/java/com/bootcamp/dscatalog/services/CategoryService.java
+++ dscatalog/back-end/dscatalog/src/main/java/com/bootcamp/dscatalog/services/CategoryService.java
@@ -3,7 +3,8 @@ package com.bootcamp.dscatalog.services;
 import com.bootcamp.dscatalog.dto.CategoryDTO;
 import com.bootcamp.dscatalog.entities.Category;
 import com.bootcamp.dscatalog.repositories.CategoryRepository;
-import com.bootcamp.dscatalog.services.exceptions.EntityNotFoundException;
+import com.bootcamp.dscatalog.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,15 +28,27 @@ public class CategoryService {
   @Transactional(readOnly = true)
   public CategoryDTO findById(Long id) {
     Optional<Category> obj = repository.findById(id);
-    Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not Found"));
+    Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not Found"));
     return new CategoryDTO(entity);
   }
 
-  @Transactional(readOnly = true)
+  @Transactional
   public CategoryDTO insert(CategoryDTO dto) {
     Category entity = new Category();
     entity.setName(dto.getName());
     entity = repository.save(entity);
     return new CategoryDTO(entity);
+  }
+
+  @Transactional
+  public CategoryDTO update(Long id, CategoryDTO dto) {
+    try {
+      Category entity = repository.getReferenceById(id);
+      entity.setName(dto.getName());
+      entity = repository.save(entity);
+      return new CategoryDTO(entity);
+    } catch (EntityNotFoundException e) {
+      throw new ResourceNotFoundException("Id not found" + id);
+    }
   }
 }
--- dscatalog/back-end/dscatalog/src/main/java/com/bootcamp/dscatalog/services/exceptions/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.bootcamp.dscatalog.services.exceptions;
-
-public class EntityNotFoundException extends RuntimeException {
-  public EntityNotFoundException(String msg) {
-    super(msg);
-  }
-}
