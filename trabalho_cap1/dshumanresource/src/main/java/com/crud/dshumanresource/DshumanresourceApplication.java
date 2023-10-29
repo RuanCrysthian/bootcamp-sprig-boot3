dc86a2cfba4b55eaad64c9a68a39f0cadf751702
Ruan Ferraz <ruan.ferraz@bethebeth.com.br>
2023-10-29 17:47:44 -0300

    build: create the project

---
+++ trabalho_cap1/dshumanresource/src/main/java/com/crud/dshumanresource/DshumanresourceApplication.java
@@ -0,0 +1,13 @@
+package com.crud.dshumanresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DshumanresourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DshumanresourceApplication.class, args);
+	}
+
+}
---
+++ trabalho_cap1/dshumanresource/src/test/java/com/crud/dshumanresource/DshumanresourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.dshumanresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DshumanresourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
